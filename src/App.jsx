43b5427b02451af9de259941c978f17e30a06475
emamaria <emascript@gmail.com>
2022-03-30 16:09:37 +0200

    chore: add adjust page limit

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ function App() {
     <div className="App">
 
 {(page > 1) &&<Button buttonName="Prev" onClick={()=> setPage(page - 1)}/>}
-  <Button buttonName="Next" onClick={()=> setPage(page + 1)}/>
-
+ {(page < 43)&&<Button buttonName="Next" onClick={()=> setPage(page + 1)}/>} 
+ 
     {
       characters&&characters.map(character => {
          return <Characters key={character.id} name={character.name}  image={character.image}/>
